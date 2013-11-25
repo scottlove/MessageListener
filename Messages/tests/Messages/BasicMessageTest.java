9e4ad5b16655d62d0fc0afd891b9ca4d528f0488
scotlov <scottlove@live.com>
2013-11-25 10:49:41 -0800

    Added new message files and tests

---
+++ Messages/tests/Messages/BasicMessageTest.java
@@ -0,0 +1,52 @@
+package Messages;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class BasicMessageTest {
+    private String testID;
+    private ArrayList<String>  testContent;
+    private BasicMessage message;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        testContent = new ArrayList<String>()  ;
+        testID = "testID";
+        for (Integer i=0;i<10;i++)
+        {
+            testContent.add(i.toString())   ;
+        }
+        message = new BasicMessage("testID")  ;
+        for (String s : testContent)
+        {
+            message.addContent(s)     ;
+        }
+
+    }
+
+    @Test
+    public void testGetMessageID() throws Exception {
+                  assertEquals(testID,message.getMessageID()) ;
+    }
+
+    @Test
+    public void testGetContent() throws Exception {
+
+        ArrayList content = (ArrayList) message.getContent();
+
+        for(int i = 0;i<testContent.size();i++)
+        {
+                assertEquals(testContent.get(i),content.get(i));
+        }
+
+    }
+
+
+}
