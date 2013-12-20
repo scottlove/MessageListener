a0afa20b8cdd58d33f8bdb5962b4378082c754d4
scotlov <scottlove@live.com>
2013-12-20 13:47:08 -0800

    refactored trace messaging

---
+++ KafkaConsumerLib/src/KafkaConsumerLib/Consumer.java
@@ -0,0 +1,38 @@
+package KafkaConsumerLib;
+
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+
+import java.util.List;
+
+
+public class Consumer implements Runnable {
+    private KafkaStream m_stream;
+    private int m_threadNumber;
+    private List<IOutputter> outputs;
+
+    public Consumer(KafkaStream a_stream, int a_threadNumber, List<IOutputter> outputters)
+    {
+        m_threadNumber = a_threadNumber;
+        m_stream = a_stream;
+        outputs = outputters;
+    }
+
+    public void run() {
+        ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
+        while (it.hasNext())
+        {
+            String out = new String(it.next().message());
+            //System.out.println(out);
+            for(IOutputter output : outputs)
+            {
+                output.writeString(out);
+            }
+        }
+
+        System.out.println("Shutting down Thread: " + m_threadNumber);
+    }
+
+
+
+}
